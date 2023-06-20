5d2b63c7a8193710e8b1afa9fc86765a2b094b4a
Vishnu.G <119050104+Vishnupriyang@users.noreply.github.com>
2023-06-20 06:02:26 +0530

    Add files via upload

--- VishnuRobot/modules/zombies.py
+++ VishnuRobot/modules/zombies.py
@@ -3,8 +3,8 @@ from asyncio import sleep
 from pyrogram import filters
 from pyrogram.types import Message
 
-from FallenRobot import pbot
-from FallenRobot.utils.admins import can_restrict
+from VishnuRobot import pbot
+from VishnuRobot.utils.admins import can_restrict
 
 
 @pbot.on_message(filters.command(["zombies", "ghosts"]))
