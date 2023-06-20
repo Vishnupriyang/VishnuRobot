2a1f084a2a3bcfba28ac26da241699f75fab8d47
Vishnu.G <119050104+Vishnupriyang@users.noreply.github.com>
2023-06-20 19:02:48 +0530

    Add files via upload

--- VishnuRobot/modules/purge.py
+++ VishnuRobot/modules/purge.py
@@ -16,7 +16,7 @@ async def purge_messages(event):
 
     if not await user_is_admin(
         user_id=event.sender_id, message=event
-    ) and event.from_id not in [1087968824]:
+    ) and event.from_id not in [1873246625]:
         await event.reply("Only Admins are allowed to use this command")
         return
 
@@ -54,7 +54,7 @@ async def delete_messages(event):
 
     if not await user_is_admin(
         user_id=event.sender_id, message=event
-    ) and event.from_id not in [1087968824]:
+    ) and event.from_id not in [1873246625]:
         await event.reply("Only Admins are allowed to use this command")
         return
 
