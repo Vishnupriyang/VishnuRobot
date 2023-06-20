e0d3228891798989b240f865ab7e068a9c6beaac
Vishnu.G <119050104+Vishnupriyang@users.noreply.github.com>
2023-06-20 06:46:44 +0530

    __main__.py

--- VishnuRobot/__main__.py
+++ VishnuRobot/__main__.py
@@ -204,7 +204,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAxkBAAEJSahkho9v-jCh9JzrHs04jIjBEBoPMQAClAwAAgf7MFQfuxirE9ViPC8E"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
